84eacf0ed32793f54b0c37ec514624bdf929f41f
waldir <nwalldir@gmail.com>
2020-09-28 23:18:12 -0400

    ajustes na no header global e responsividade

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -11,32 +11,78 @@ export class ContactComponent implements OnInit {
   isDesktop: boolean;
   contactForm: FormGroup
   emailFormControl: FormControl;
-  constructor(private formBuilder: FormBuilder, private breakpointObserver: BreakpointObserver) { }
+  xsmall = false;
+  small = false;
+  medium = false;
+  large = false;
+  xlarge = false;
+  medium_small = true;
+  constructor(private breakpointObserver: BreakpointObserver, private formBuilder: FormBuilder) { }
+
   ngOnInit() {
-    // 1256
-    //760
     this.breakpointObserver.observe([
-      '(min-width: 375px)',
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
     ]).subscribe((result) => {
 
-      if (result.matches) {
-        this.isDesktop = true;
-      } else {
-        this.isDesktop = false;
-      }
-      console.log(this.isDesktop);
-    })
-    this.breakpointObserver.observe([
-      '(min-width: 760px)',
-    ]).subscribe((result) => {
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        // handle XSmall breakpoint
+        this.xsmall = true;
 
-      if (result.matches) {
-        this.isDesktop = true;
-      } else {
-        this.isDesktop = false;
+        this.small = false;
+        this.medium = false;
+        this.large = false;
+        this.xlarge = false;
+        this.medium_small = true;
       }
-      console.log(`760,`, this.isDesktop);
+      if (result.breakpoints[Breakpoints.Small]) {
+        this.small = true;
+
+        this.xsmall = false;
+        this.medium = false;
+        this.large = false;
+        this.xlarge = false;
+        this.medium_small = true;
+        console.log('Small');
+        // handle Small breakpoint
+      }
+      if (result.breakpoints[Breakpoints.Medium]) {
+        this.medium = true;
+
+        this.small = false;
+        this.xsmall = false;
+        this.large = false;
+        this.xlarge = false;
+        this.medium_small = true;
+        console.log('Medium');
+        // handle Medium breakpoint
+      }
+      if (result.breakpoints[Breakpoints.Large]) {
+        this.large = true;
+
+        this.medium = false;
+        this.small = false;
+        this.xsmall = false;
+        this.xlarge = false;
+        this.medium_small = false;
+        // handle Large breakpoint
+      }
+      if (result.breakpoints[Breakpoints.XLarge]) {
+        this.xlarge = true;
+
+        this.medium = false;
+        this.small = false;
+        this.xsmall = false;
+        this.large = false;
+        this.medium_small = false;
+        console.log('XLarge');
+        // handle XLarge breakpoint
+      }
     })
+
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', Validators.required],
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,22 +7,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  isDesktop: boolean;
+  xsmall = false;
+  small = false;
+  medium = false;
+  large = false;
+  xlarge = false;
 
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
     this.breakpointObserver.observe([
-      '(min-width: 757px)'
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
     ]).subscribe((result) => {
 
-      if (result.matches) {
-        this.isDesktop = true;
-      } else {
-        this.isDesktop = false;
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        // handle XSmall breakpoint
+        this.xsmall = true;
+
+        this.small = false;
+        this.medium = false;
+        this.large = false;
+        this.xlarge = false;
       }
-      console.log(this.isDesktop);
+      if (result.breakpoints[Breakpoints.Small]) {
+        this.small = true;
+
+        this.xsmall = false;
+        this.medium = false;
+        this.large = false;
+        this.xlarge = false;
+        console.log('Small');
+        // handle Small breakpoint
+      }
+      if (result.breakpoints[Breakpoints.Medium]) {
+        this.medium = true;
+
+        this.small = false;
+        this.xsmall = false;
+        this.large = false;
+        this.xlarge = false
+        console.log('Medium');
+        // handle Medium breakpoint
+      }
+      if (result.breakpoints[Breakpoints.Large]) {
+        this.large = true;
+
+        this.medium = false;
+        this.small = false;
+        this.xsmall = false;
+        this.xlarge = false;
+        // handle Large breakpoint
+      }
+      if (result.breakpoints[Breakpoints.XLarge]) {
+        this.xlarge = true;
+
+        this.medium = false;
+        this.small = false;
+        this.xsmall = false;
+        this.large = false;
+        console.log('XLarge');
+        // handle XLarge breakpoint
+      }
     })
   }
-
 }
